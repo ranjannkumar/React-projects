cafb8c63663b18097d98a49e974201337b1ef26b
Ranjan kumar <ranjankumar5673@gmail.com>
2024-06-17 09:40:56 +0530

    Todos project completed

--- reduxToolkit/src/app/store.js
+++ reduxToolkit/src/app/store.js
@@ -1,3 +1,5 @@
 import {configureStore} from '@reduxjs/toolkit'
-
-export const store=configureStore({});
+import todoReducer from '../features/todo/todoSlice'
+export const store=configureStore({
+  reducer: todoReducer
+});
